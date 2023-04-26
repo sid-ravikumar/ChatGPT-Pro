a337c559a6a2d8b31f3749b8bcc2ec1389cc65a7
Siddharth Ravikumar <ravikumar.siddharth@gmail.com>
2023-04-26 00:04:50 -0400

    Improved appearance, count persists across chats

--- content.js
+++ content.js
@@ -27,22 +27,30 @@ function countTokens(text) {
 function injectStyles() {
   const style = document.createElement('style');
   style.textContent = `
-    .token-counter-wrapper::after {
-      content: attr(data-token-count);
+    .token-counter-wrapper {
+      position: relative;
+    }
+    .token-counter {
       position: absolute;
-      bottom: 5px;
-      right: 10px;
-      font-size: 12px;
-      color: #999;
+      bottom: 8px;
+      right: 50px; // Increase this value if needed to position the token count further to the left
+      font-size: 14px;
+      font-weight: bold;
+      color: #333;
+      background-color: #f8f9fa;
+      padding: 2px 6px;
+      border-radius: 3px;
     }
   `;
   document.head.appendChild(style);
 }
 
+
 function updateTokenCount(wrapper) {
   const textarea = wrapper.querySelector('textarea');
   const tokenCount = countTokens(textarea.value);
-  wrapper.setAttribute('data-token-count', `${tokenCount} tokens`);
+  const tokenCounter = wrapper.querySelector('.token-counter');
+  tokenCounter.textContent = `${tokenCount} tokens`;
 }
 
 function initTokenCounter() {
@@ -53,17 +61,49 @@ function initTokenCounter() {
     return;
   }
 
+  // Check if the token counter is already initialized
+  if (textarea.parentElement.classList.contains('token-counter-wrapper')) {
+    return;
+  }
+
   injectStyles();
 
   const wrapper = document.createElement('div');
   wrapper.classList.add('token-counter-wrapper');
-  wrapper.style.position = 'relative';
+
+  const tokenCounter = document.createElement('div');
+  tokenCounter.classList.add('token-counter');
 
   textarea.parentElement.insertBefore(wrapper, textarea);
   wrapper.appendChild(textarea);
+  wrapper.appendChild(tokenCounter);
 
   updateTokenCount(wrapper);
   textarea.addEventListener('input', () => updateTokenCount(wrapper));
 }
 
+
+// Observe changes in the application to reinitialize the token counter
+function observeAppChanges() {
+  const appRoot = document.querySelector('#__next');
+
+  if (!appRoot) {
+    setTimeout(observeAppChanges, 500);
+    return;
+  }
+
+  const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      if (mutation.type === 'childList' || mutation.type === 'subtree') {
+        initTokenCounter();
+        break;
+      }
+    }
+  });
+
+  observer.observe(appRoot, { childList: true, subtree: true });
+}
+
+// Initialize the token counter and start observing changes
 initTokenCounter();
+observeAppChanges();
